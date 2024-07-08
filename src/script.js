b475abf66b4db1e9c82f97550641e0d910d501aa
Aravindvenkat19 <146709026+Aravindvenkat19@users.noreply.github.com>
2024-07-08 05:41:39 +0000

    implemented few calculations for scientific calculator

--- src/script.js
+++ src/script.js
@@ -1,36 +1,53 @@
 let currentValue = "";
 
-    const display = document.getElementById("calc-input")
-    const buttons = document.getElementsByClassName("btn")
+const display = document.getElementById("calc-input")
+const buttons = document.getElementsByClassName("btn")
 
-    function equaltoResult(){
-      console.log('currentValue:',currentValue)
-      const convertedValue = currentValue.replace("x","*")
-                                         .replace("÷","/")
-                                         .replace("%","*0.01")
-      const result = eval(convertedValue);
-      console.log('convertedValue:',convertedValue)
-      currentValue = result.toString();
-      display.value = currentValue
+function equaltoResult() {
+  console.log('currentValue:', currentValue)
+  const convertedValue = currentValue.replace("x", "*")
+    .replace("÷", "/")
+    .replace("%", "*0.01")
+    .replace("sin", "Math.sin")
+    .replace("cos", "Math.cos")
+    .replace("ln", "Math.log")
+    .replace("π", "Math.PI")
+    .replace("log", "Math.log10")
+    .replace("e", "Math.E")
+    .replace("tan", "Math.tan")
+    .replace("√", "Math.sqrt")
+  const result = eval(convertedValue);
+  console.log('convertedValue:', convertedValue)
+  currentValue = result.toString();
+  display.value = currentValue
+}
+
+
+for (let i = 0; i < buttons.length; i++) {
+  const button = buttons[i]; //getting each buttons as button i
+  button.addEventListener("click", function () {   //adding click event to each buttons 
+    const value = button.innerText;
+
+    try {
+
+
+      if (value == "AC") {
+        currentValue = "";
+        display.value = currentValue
+      } else if (value == "=") {
+        equaltoResult()
+      }
+      else {
+        currentValue += value;
+        display.value = currentValue;
+      }
+
+    } catch (error) {
+        currentValue = "Error Value...!!!"
+        console.error(error)
+        display.value = currentValue
     }
 
-   
-    for(let i=0;i<buttons.length;i++){
-        const button = buttons[i]; //getting each buttons as button i
-            button.addEventListener("click",function(){   //adding click event to each buttons 
-            const value = button.innerText;
-        
-        if(value == "AC"){
-           currentValue = "";
-           display.value = currentValue
-        }else if (value == "="){
-          equaltoResult()
-        }
-        else{
-            currentValue += value;
-            display.value = currentValue;
-        }
-            
 
-        })
-    }
+  })
+}
